bca864b2e59e56b150e892259d67988884e23c2e
gopal-sakshi <gopal612@gmail.com>
2022-10-31 17:25:50 +0530

    web sockets

--- index.js
+++ index.js
@@ -1,25 +1,69 @@
+// import uuid4 from "uuid4";
+const uuidv4 = require('uuid4');
 const express = require('express');
 const ws = require('ws');
+const clients = new Map();
 
 const app = express();
 const port = 3064;
 
 const wsServer = new ws.Server({ noServer: true });
-wsServer.on('connection', socket => {
-    socket.on('message', message => {
 
-        wsServer.clients.forEach(function each(client) {
-            if (client.readyState === ws.OPEN) {
-                setTimeout(function () {
-                    client.send(
-                        Buffer.from(JSON.stringify({ "source": "server", "content": "response from server" })), 
-                        { binary: false });
-                }, 1000);
-            }
-        });
-        console.log(message.toString());
+const cb3 = function() {
+    console.log(`client disconnected`);
+}
+
+const cb2 = function (message) {
+    // wsServer.clients.forEach(function each(client) {
+    //     if (client.readyState === ws.OPEN) {        
+    //         var serverResponse = {
+    //             source: "server", 
+    //             content: `your message "${message.content}" received bhai`,
+    //             timeStamp: new Date()
+    //         }
+    //         client.send(Buffer.from(JSON.stringify(serverResponse)), { binary: false });            
+    //     }
+    // });
+    // console.log(message.toString());
+}
+const cb1 = function (webSocketClientInstance) {
+    const id = uuidv4();        // Every time a client connects, we generate a new unique ID,
+    const color = Math.floor(Math.random() * 360);
+    const metadata = { id, color };
+    clients.set(webSocketClientInstance, metadata);
+
+    // message Event is on webSocketClientInstance & not on wsServer itself
+        // anyway, whenever the client receives a message ----> cb2 is triggered
+    webSocketClientInstance.on('message', message => {
+        console.log('message --------> ',message);
+        console.log('parsed msg -----> ',JSON.parse(message));
+        const metadata = clients.get(webSocketClientInstance);
+        console.log(metadata);
+        console.log(wsServer.clients);
+        // console.log(metadata.sender);
+        // console.log(metadata.content);
     });
-});
+
+    webSocketClientInstance.on('close', cb3);
+}
+
+
+
+/******************************************************************************************************** */
+// connection event ----> cb1 callback gets triggered whenever a new webSocket client connects to server (testing angular app (or) postman)
+//                  ----> cb1 callback gets triggered which accepts a parameter, which is nothing but webSocketClientInstance
+wsServer.on('connection', cb1);
+/******************************************************************************************************** */
+
+
+
+
+
+
+
+
+
+/******************************************************************************************************** */
 
 // `server` is a vanilla Node.js HTTP server, so use
 // the same ws upgrade process described here:
@@ -34,3 +78,4 @@ server.on('upgrade', (request, socket, head) => {
         wsServer.emit('connection', socket, request);
     });
 });
+/******************************************************************************************************** */
