19a4ec79e3ca4c6f74e008e9e832234cd1157c7c
gopal-sakshi <gopala.sakshi@vasudhaika.net>
2023-03-13 06:54:37 +0530

    ss

--- index1.js
+++ index1.js
@@ -1,4 +1,3 @@
-// import uuid4 from "uuid4";
 const uuidv4 = require('uuid4');
 const express = require('express');
 const ws = require('ws');
@@ -9,9 +8,7 @@ const port = 3064;
 
 const wsServer = new ws.Server({ noServer: true });
 
-const cb3 = function() {
-    console.log(`client disconnected`);
-}
+const cb3 = function() { console.log(`client disconnected`); }
 
 const cb2 = function (message) {
     // wsServer.clients.forEach(function each(client) {
@@ -27,7 +24,7 @@ const cb2 = function (message) {
     // console.log(message.toString());
 }
 const cb1 = function (webSocketClientInstance) {
-    const id = uuidv4();        // Every time a client connects, we generate a new unique ID,
+    const id = uuidv4();
     const color = Math.floor(Math.random() * 360);
     const metadata = { id, color };
     clients.set(webSocketClientInstance, metadata);
@@ -40,11 +37,7 @@ const cb1 = function (webSocketClientInstance) {
         console.log('parsed msg -----> ',JSON.parse(message));
         const metadata = clients.get(webSocketClientInstance);
         console.log(metadata);
-        // console.log(wsServer.clients);
-        // console.log(metadata.sender);
-        // console.log(metadata.content);
     });
-
     webSocketClientInstance.on('close', cb3);
 }
 
@@ -54,29 +47,15 @@ const cb1 = function (webSocketClientInstance) {
 // connection event ----> cb1 callback gets triggered whenever a new webSocket client connects to server (testing angular app (or) postman)
 //                  ----> cb1 callback gets triggered which accepts a parameter, which is nothing but webSocketClientInstance
 wsServer.on('connection', cb1);
-/******************************************************************************************************** */
 
-
-
-
-
-
-
-
-
-/******************************************************************************************************** */
 
 // `server` is a vanilla Node.js HTTP server, so use
 // the same ws upgrade process described here:
 // https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
 
-const server = app.listen(port, () => { 
-    console.log(`Example app listening at http://localhost:${port}`)
-    });
+const server = app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`) });
 
 server.on('upgrade', (request, socket, head) => {
-    wsServer.handleUpgrade(request, socket, head, socket => {
-        wsServer.emit('connection', socket, request);
-    });
+    wsServer.handleUpgrade(request, socket, head, socket => { wsServer.emit('connection', socket, request); });
 });
 /******************************************************************************************************** */
