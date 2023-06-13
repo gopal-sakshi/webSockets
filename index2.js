9b70e292f2dd0850d56f9b485b68cefd3be1f273
gopal-sakshi <gopal612@gmail.com>
2023-06-13 11:18:40 +0530

    making webSockets work

--- index2.js
+++ index2.js
@@ -2,19 +2,37 @@ var mongoose = require('mongoose');
 var dbUrl = require('./config/mongo-config');
 var express = require('express');
 var app = express();
-var server = app.listen(3065, () => {
-    console.log('server is running on port', server.address().port);
-});
+
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
+
+
+/* 
+    This will fail ===> 
+    (a) since socket.io uses http server and not the express server. 
+    (b) you need to listen on http server but not on express like app.listen()
+    (c) change app.listen() to server.listen()
+        because express requires that you instantiate a HTTP server in Socket.IO
+
+*/
+
+// FAILS
+// var server = app.listen(3065, () => {
+//     console.log('server is running on port', server.address().port);
+// });
+// DOESNT FAIL
+http.listen(3065, () => {
+    console.log('server is running on port 3065');
+})
+
 var io = require('socket.io')(http);
 io.on('connection', () =>{
     console.log('a user is connected');
 })
 /*************************************************************************************** */
-// mongoose.connect(dbUrl , (err) => { 
-//     console.log('mongodb connected ',err);
-// })
+mongoose.connect(dbUrl , (err) => { 
+    console.log('mongodb connected ',err);
+});
 var Message = mongoose.model(`Message`,{ name : String, message : String});
 /*************************************************************************************** */
 
@@ -34,21 +52,15 @@ app.use(express.static(__dirname));
 
 /*********************** ROUTES *************************************************** */
 app.get('/messages', (req, res) => {
-    // Message.find({},(err, messages)=> {
-    //   res.send(messages);
-    // })
-    console.log(req.body);
-    res.send({"hello":"doctor"});
+    Message.find({},(err, messages)=> {
+        res.send(messages);
+    });
 });
 
 app.post('/messages', (req, res) => {
-    console.log(req.body);
-    res.send('waittttu');
     var message = new Message(req.body);
     message.save((err) =>{
-        if(err) {
-            res.sendStatus(500);
-        }        
+        if(err) { res.sendStatus(500); }
         io.emit('message', req.body);
         res.sendStatus(200);
     })
